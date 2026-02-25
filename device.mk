fe26cbb2b3c921fe6782e1dcf4cbe1b58f4199f7
R0X <patryk.bieganski1003@gmail.com>
2026-02-25 02:07:58 +0100

    Add exFAT utilities to PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -43,3 +43,8 @@ TARGET_SCREEN_HEIGHT := 2400
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat
