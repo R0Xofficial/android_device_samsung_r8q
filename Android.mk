f639832a4fabdba3fdaf3eb5b29445e733e5df64
R0X <patryk.bieganski1003@gmail.com>
2026-02-26 12:55:46 +0100

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022-2025 The Android Open Source Project
+# Copyright (C) 2022-2025 The TeamWin Recovery Project
+# Copyright (C) 2025 OrangeFox Recovery Project
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),r8q)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
