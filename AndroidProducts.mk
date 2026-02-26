627694d41dfe3bb749eb055f2e321e09b5408cc0
R0X <patryk.bieganski1003@gmail.com>
2026-02-26 12:54:24 +0100

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022-2025 The Android Open Source Project
+# Copyright (C) 2022-2025 The TeamWin Recovery Project
+# Copyright (C) 2025 OrangeFox Recovery Project
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_r8q.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_r8q-eng
