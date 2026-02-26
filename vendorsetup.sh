3d09284616082d16c909a823b8c58cdd1daecbe3
R0X <patryk.bieganski1003@gmail.com>
2026-02-26 21:03:18 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -30,7 +30,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		export LC_ALL="C"
 		
 		# OrangeFox Core Settings
-		export FOX_VANILLA_BUILD=1
+		export OF_VANILLA_BUILD=1
 		export FOX_BUILD_TYPE="Unofficial"
 		export OF_MAINTAINER="R0Xofficial"
         export OF_MAINTAINER_AVATAR="$PWD/device/samsung/r8q/maintainer.png"
