e05151b9b55a3dbf40dca1faf456eb0eab3dc18c
R0X <patryk.bieganski1003@gmail.com>
2026-02-26 21:36:01 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,35 +1,41 @@
 #
-# Copyright (C) 2022-2025 The TeamWin Recovery Project
-# Copyright (C) 2025 OrangeFox Recovery Project
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_SHIPPING_API_LEVEL := 29
-
+# Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl.recovery
-
+# qcom decryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
-PRODUCT_PACKAGES += \
-    mount.exfat \
-    fsck.exfat \
-    mkfs.exfat
-
+# Dependencies
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
-    libdmabufheap \
-    libfs_mgr_erofs
+    libdmabufheap
 
-PRODUCT_TARGET_VNDK_VERSION := 30
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so
+
+# Screen
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2400
