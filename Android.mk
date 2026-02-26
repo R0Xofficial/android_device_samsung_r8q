65acbbe8e0eba1620dae92d4181f96868f35ff0d
R0X <patryk.bieganski1003@gmail.com>
2026-02-26 21:37:04 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022-2025 The Android Open Source Project
-# Copyright (C) 2022-2025 The TeamWin Recovery Project
-# Copyright (C) 2025 OrangeFox Recovery Project
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),r8q)
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
+include $(CLEAR_VARS)
 endif
