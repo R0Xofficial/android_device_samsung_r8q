cab528ee0e656a43c1486ba96c998318560d5cf4
R0X <patryk.bieganski1003@gmail.com>
2026-03-03 21:20:04 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,7 +126,6 @@ TARGET_OTA_ASSERT_DEVICE := r8q
 TARGET_USES_MKE2FS := true
 
 # TWRP specific build flags
-TW_DEVICE_VERSION := R0Xofficial
 TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
