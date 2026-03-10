1d2ee1456c0200ea7f2e8aeeca19b14f41b96a5d
R0X <patryk.bieganski1003@gmail.com>
2026-03-10 18:00:37 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,10 +22,10 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export ALLOW_MISSING_DEPENDENCIES=true
     export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-    export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
-    export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
-    export LC_ALL="C"
+    export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+    export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+    export LC_ALL=C
     
     export OF_MAINTAINER="R0Xofficial"
     export FOX_BUILD_TYPE="Unofficial"
@@ -43,20 +43,20 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export FOX_DISABLE_APP_MANAGER=1
     export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
     
-    export OF_SCREEN_H="2400"
-    export OF_STATUS_H="72"
-    export OF_STATUS_INDENT_LEFT="48"
-    export OF_STATUS_INDENT_RIGHT="48"
+    export OF_SCREEN_H=2400
+    export OF_STATUS_H=72
+    export OF_STATUS_INDENT_LEFT=48
+    export OF_STATUS_INDENT_RIGHT=48
     export OF_HIDE_NOTCH=1
     export OF_CLOCK_POS=1
     export OF_ALLOW_DISABLE_NAVBAR=0
     export OF_USE_GREEN_LED=0
 
-    export FOX_USE_NANO_EDITOR="1"
-    export FOX_DELETE_AROMAFM="1"
-    export FOX_USE_TAR_BINARY="1"
-    export FOX_USE_SED_BINARY="1"
-    export FOX_USE_XZ_UTILS="1"
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_DELETE_AROMAFM=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_SED_BINARY=1
+    export FOX_USE_XZ_UTILS=1
 
 	export OF_FLASHLIGHT_ENABLE=1
 	export OF_FL_PATH1="/system/flashlight"
