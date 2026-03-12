4ecebd75d00bf2599e542e752a95bda4fcaed3be
R0X <patryk.bieganski1003@gmail.com>
2026-03-12 17:10:03 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,4 @@
 export ALLOW_MISSING_DEPENDENCIES=true
+export PB_DISABLE_DEFAULT_DM_VERITY=true
+export PB_TORCH_PATH="/sys/devices/virtual/camera/flash/rear_flash"
+export PB_TORCH_MAX_BRIGHTNESS=1
