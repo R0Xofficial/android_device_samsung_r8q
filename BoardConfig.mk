c3eb65922e03e5ee9c04c46475aa7e4497359f5f
R0X <patryk.bieganski1003@gmail.com>
2026-03-12 21:22:51 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,6 +128,7 @@ TARGET_OTA_ASSERT_DEVICE := r8q
 TARGET_USES_MKE2FS := true
 
 # TWRP specific build flags
+TW_MAINTAINER := R0Xofficial
 TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
@@ -160,3 +161,8 @@ TW_INCLUDE_RESETPROP := true
 # CPUset
 ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
+
+# PBRP specific build flags
+PB_DISABLE_DEFAULT_DM_VERITY := true
+PB_TORCH_PATH := "/sys/devices/virtual/camera/flash/rear_flash"
+PB_TORCH_MAX_BRIGHTNESS := 1
