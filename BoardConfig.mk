695ef4733a5010a0be269e01f71a413fe537ba56
R0X <patryk.bieganski1003@gmail.com>
2026-03-24 11:19:32 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ BOARD_KERNEL_CMDLINE := \
     swiotlb=2048 \
     printk.devkmsg=on \
     firmware_class.path=/vendor/firmware_mnt/image \
-    loop.max_part=16 \
+    loop.max_part=32 \
     msm_ss.restart_level=1 \
     panic=0
 
