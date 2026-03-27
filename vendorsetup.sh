31272a33f296394ef5a8c3053750ce8e29757383
R0X <patryk.bieganski1003@gmail.com>
2026-03-27 19:52:12 +0100

    addons: Disabled experimental FRP bypass addon

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -57,7 +57,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export FOX_USE_TAR_BINARY=1
     export FOX_USE_SED_BINARY=1
     export FOX_USE_XZ_UTILS=1
-	export OF_ENABLE_FRP_ADDON=1
 
 	export OF_FLASHLIGHT_ENABLE=1
 	export OF_FL_PATH1="/system/flashlight"
